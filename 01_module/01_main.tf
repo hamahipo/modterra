e9fe3ce3d16faadcdc4910163ff69e4abb1b32a0
yljo <noppy1004@gmail.com>
2021-11-01 12:11:32 +0900

    directory

--- 01_module/01_main.tf
+++ 01_module/01_main.tf
@@ -1,3 +1,17 @@
 provider "aws" {
   region = "ap-northeast-2"
 }
+
+resource "aws_key_pair" "yljo_key" {
+  key_name   = "tf-key1"
+  public_key = file("../../../.ssh/id_rsa.pub")
+}
+
+resource "aws_vpc" "yljo_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+      Name   =  "yljo-vpc"  
+  }
+}
